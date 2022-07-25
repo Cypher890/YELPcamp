da7e8247830a76d69ed8de30a55951eb5ff06d01
Cypher890 <abdulsuhaib07@gmail.com>
2022-07-25 19:15:49 +0530

    added new.ejs now we can add location

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -17,6 +17,8 @@ const app = express()
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname,'views'))
 
+app.use(express.urlencoded({ extended: true}))
+
 // app.get('/',(req,res) => {
 //     res.send('HELLO FROM YELP CAMP!')
 // })
@@ -29,10 +31,22 @@ app.get('/campgrounds',async(req,res) => {
     res.render('campgrounds/index',{ campgrounds })
 })
 
+app.get('/campgrounds/new', (req,res) =>{
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async(req,res) =>{
+    const campground = new Campground(req.body.campground)
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
 app.get('/campgrounds/:id', async(req,res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground })
 })
+
+
 /*app.get('/makecampground',async (req,res) => {
     const camp = new Campground({ title: 'My Backyard', description: 'cheap camping!'})
     await camp.save()
