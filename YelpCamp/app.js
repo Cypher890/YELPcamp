70b60edc6717ea3394af2bbdcdc41b02a50e64fa
Cypher890 <abdulsuhaib07@gmail.com>
2022-08-18 15:56:54 +0530

    made a register form and fixed validation error

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -11,9 +11,10 @@ const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 
 
-
-const campgrounds = require('./routes/campgrounds')
-const reviews = require('./routes/reviews')
+const userRoutes = require('./routes/users')
+const campgroundRoutes = require('./routes/campgrounds')
+const reviewRoutes = require('./routes/reviews')
+const { application } = require('express')
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
 
@@ -74,9 +75,9 @@ app.get('/fakeUser', async (req,res) => {
 // })
 
 
-
-app.use('/campgrounds', campgrounds)
-app.use('/campgrounds/:id/reviews', reviews)
+app.use('/',userRoutes)
+app.use('/campgrounds', campgroundRoutes)
+app.use('/campgrounds/:id/reviews', reviewRoutes)
 
 
 
