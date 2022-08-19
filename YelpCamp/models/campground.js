00f5d0a63554d35e9383ee1ddaee45c729250b58
Cypher890 <abdulsuhaib07@gmail.com>
2022-08-19 17:47:41 +0530

    added author to the campgrounds

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -8,7 +8,10 @@ const CampgroundSchema = new Schema({
     price: Number,
     description: String,
     location: String,
-    reviews: String,
+    author:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
     reviews: [
         {
             type: Schema.Types.ObjectId,
