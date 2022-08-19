e43a91700f1a4a2c5fc2487f3a3231e984aa5acb
Cypher890 <abdulsuhaib07@gmail.com>
2022-08-19 19:09:02 +0530

    moved the middleware for validation to middleware file

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -1,31 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
-const ExpressError = require('../utils/ExpressError')
+
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
+
 const Campground = require('../models/campground')
-const {campgroundSchema}= require('../schemas.js')
-const {isLoggedIn} = require('../middleware')
 
 
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body)
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next()
-    }
-}
-
-const isAuthor = async (req, res, next) =>{
-    const { id } = req.params 
-    const campground = await Campground.findById(id)
-    if (!campground.author.equals(req.user._id)){
-        req.flash('error', 'You do not have permission to do that')
-        return res.redirect(`/campgrounds/${id}`)
-    }
-    next()
-}
 
 
 router.get('/', catchAsync(async (req, res) => {
@@ -33,50 +14,50 @@ router.get('/', catchAsync(async (req, res) => {
     res.render('campgrounds/index', { campgrounds })
 }))
 
-router.get('/new',isLoggedIn, (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new')
 })
 
-router.post('/',isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
+router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     //if(!req.body.campground) throw new ExpressError('Invalid Campground Data', 400)    
     const campground = new Campground(req.body.campground)
     campground.author = req.user._id
     await campground.save()
-    req.flash('success','Successfully made a new campground!')
+    req.flash('success', 'Successfully made a new campground!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.get('/:id',catchAsync(async (req, res) => {
+router.get('/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews').populate('author')
-    if(!campground){
-        req.flash('error','Cannot find that campground')
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground')
         return res.redirect('/campgrounds')
     }
     res.render('campgrounds/show', { campground })
 }))
 
-router.get('/:id/edit',isLoggedIn, isAuthor, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params
-    const campground = await Campground.findById(id)    
-    if(!campground){
-        req.flash('error','Cannot find that campground')
+    const campground = await Campground.findById(id)
+    if (!campground) {
+        req.flash('error', 'Cannot find that campground')
         return res.redirect('/campgrounds')
     }
-    
+
     res.render('campgrounds/edit', { campground })
 }))
-router.put('/:id',isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params
-   
+
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
-    req.flash('success','Successfully updated campground!!!')
+    req.flash('success', 'Successfully updated campground!!!')
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete('/:id',isLoggedIn, isAuthor , catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params
     await Campground.findByIdAndDelete(id)
-    req.flash('success','Successfully deleted campground')
+    req.flash('success', 'Successfully deleted campground')
     res.redirect('/campgrounds')
 }))
 
