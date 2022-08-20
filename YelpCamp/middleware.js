7468a8e6da0bd44fd43a18b0a845a3b5f3e1d600
Cypher890 <abdulsuhaib07@gmail.com>
2022-08-20 15:37:47 +0530

    refactored the routes in a fancy way and fixed the redirect error by adding keepsessioninfo:true to post route of users

--- YelpCamp/middleware.js
+++ YelpCamp/middleware.js
@@ -9,7 +9,7 @@ module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
         req.session.returnTo = req.originalUrl
         req.flash('error','you must be signed in')
-       return res.redirect('/login')
+        return res.redirect('/login')
     }
     next()
 }
