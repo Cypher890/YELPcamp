167b49c330aec211556b35bad55ebb3d6ab1fe02
Cypher890 <abdulsuhaib07@gmail.com>
2022-08-26 12:16:49 +0530

    stored uploded image links in mongo

--- YelpCamp/schemas.js
+++ YelpCamp/schemas.js
@@ -4,7 +4,7 @@ module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().required(),
+        //image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
     }).required()
@@ -12,7 +12,7 @@ module.exports.campgroundSchema = Joi.object({
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().required(),
+        rating: Joi.number().required().min(1).max(5),
         body: Joi.string().required()
     }).required()
 })
