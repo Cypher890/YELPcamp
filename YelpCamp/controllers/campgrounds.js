c1e95f3ea6ece9f89cbb40c9dde8f0b01cecb3cd
Cypher890 <abdulsuhaib07@gmail.com>
2022-08-26 16:46:15 +0530

    created a delete form where we can delete images

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -34,6 +34,7 @@ module.exports.createCampground = async (req, res, next) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params
+    console.log(req.body)
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground })
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename}))
     campground.images.push(...imgs)
