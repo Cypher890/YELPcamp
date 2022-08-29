2b56c34db07b0e5fd4f3d4de4fea8f3ca4c565df
Cypher890 <abdulsuhaib07@gmail.com>
2022-08-29 15:08:04 +0530

    deleted all campground while messing with seed and map works fine

--- YelpCamp/public/javascripts/showPageMap.js
+++ YelpCamp/public/javascripts/showPageMap.js
@@ -2,10 +2,13 @@ mapboxgl.accessToken = mapToken;
     const map = new mapboxgl.Map({
       container: 'map', 
       style: 'mapbox://styles/mapbox/streets-v11', 
-      center: [-74.5, 40], 
-      zoom: 6, 
+      center: campground.geometry.coordinates, 
+      zoom: 8, 
       projection: 'globe' 
     });
     map.on('style.load', () => {
       map.setFog({}); // Set the default atmosphere style
     });
+     new mapboxgl.Marker()
+      .setLngLat(campground.geometry.coordinates)
+      .addTo(map);
