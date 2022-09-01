c8b32f62e33e23c41b111619156e192e1390cb56
Cypher890 <abdulsuhaib07@gmail.com>
2022-09-01 12:10:49 +0530

    sanitized the html with joi and prevented mongo injection,cross site scripting

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const methodOverride = require('method-override')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
+const mongoSanitize = require('express-mongo-sanitize');
 
 
 const userRoutes = require('./routes/users')
@@ -43,6 +44,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(mongoSanitize())
 
 const sessionConfig = {
     secret:'thisshouldbeabettersecret',
@@ -67,7 +69,7 @@ passport.deserializeUser(User.deserializeUser())
 
 
 app.use((req, res, next) => {
-    // console.log(req.session)
+    //console.log(req.query)
     res.locals.currentUser = req.user
     res.locals.success = req.flash('success')
     res.locals.error = req.flash('error')
