04d53dfd45cdf064dc1588d08ce2fa7448b7282a
Cypher890 <abdulsuhaib07@gmail.com>
2022-09-01 17:25:58 +0530

    used helmet for security, added my srcs for csp otherwise my app would break

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
- if (process.env.NODE_ENV !== "production"){
-     require('dotenv').config()
- }
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config()
+}
 //require('dotenv').config()
 //console.log(process.env.SECRET)
 
@@ -17,6 +17,7 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet')
 
 
 const userRoutes = require('./routes/users')
@@ -47,8 +48,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize())
 
 const sessionConfig = {
-    name:'session',
-    secret:'thisshouldbeabettersecret',
+    name: 'session',
+    secret: 'thisshouldbeabettersecret',
     resave: false,
     saveUninitialized: true,
     cookies: {
@@ -61,6 +62,66 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(helmet({
+    //contentSecurityPolicy: false,
+    crossOriginResourcePolicy:false,
+    crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy:false,
+
+}))
+
+
+// helmet policy for security starts from here
+
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://code.jquery.com/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css",
+];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/cypher12/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
+// csp regulation
+ 
 
 app.use(passport.initialize())
 app.use(passport.session())
@@ -81,7 +142,7 @@ app.use((req, res, next) => {
 
 
 
-app.use('/',userRoutes)
+app.use('/', userRoutes)
 app.use('/campgrounds', campgroundRoutes)
 app.use('/campgrounds/:id/reviews', reviewRoutes)
 
